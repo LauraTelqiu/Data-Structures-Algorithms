18f0e1fe126cd11551773142077ec9c4a5e7b8cc
Laura <lauratelqiu@gmail.com>
2022-10-29 11:53:10 -0400

    mergesort

--- Algorithms/sorting/insertSort.js
+++ Algorithms/sorting/insertSort.js
@@ -10,7 +10,7 @@ function insertionSort(array) {
       // only sort number smaller than number on the left of it. This is the part of insertion sort that makes it fast if the array is almost sorted.
       if (array[i] < array[i - 1]) {
         //find where number should go
-        for (var j = 1; j < i; j++) {
+        for (let j = 1; j < i; j++) {
           if (array[i] >= array[j - 1] && array[i] < array[j]) {
             //move number to the right spot
             array.splice(j, 0, array.splice(i, 1)[0]);
